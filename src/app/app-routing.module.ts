2e66908292e03d0f7373dc153a2ccfe7d7955bd0
ba2dzc <batu.duzcu@gmail.com>
2022-08-23 18:25:06 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { OnWorkComponent } from './onWork/onWork.component';
+import { PriceComponent } from './Price/Price.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'calisma',component:OnWorkComponent},
+  {path:'price',component:PriceComponent},
+  {path:'login',component:AuthComponent},
+  {path:'home',component:HomeComponent},
+  {path:'',redirectTo:'home',pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { OnWorkComponent } from './onWork/onWork.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [					
+    AppComponent,
+      NavbarComponent,
+      HomeComponent,
+      AuthComponent,
+      OnWorkComponent,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
